19647d60c1b504ef111600227fa99f417013559b
Şamil Korkmaz <samil.korkmaz@gmail.com>
2019-05-30 09:23:36 +0300

    Added batchFileAlreadyRunning check

--- runBatFileAndSendFileToClient.js
+++ runBatFileAndSendFileToClient.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var child_process = require('child_process');
 
 const batchFilePath = "test.bat";
+var batchFileAlreadyRunning = false;
 
 const server = http.createServer(function (req, res) {
     console.log(': ' + req.url);
@@ -21,33 +22,42 @@ const server = http.createServer(function (req, res) {
                 });
             break;
         case '/getFile':
-            //run bat file: 
-            //https://stackoverflow.com/a/36601805/51358           
-            child_process.exec(batchFilePath, function (error, stdout, stderr) {
-                console.log(stdout);
-                if (error) {
-                    const msg = 'Error executing ' + batchFilePath + ".Error: " + error;
-                    console.log(msg);
-                    res.writeHead(500);
-                    return res.end(msg);
-                }
+            if(batchFileAlreadyRunning) {
+                const msg = "Batch file " + batchFilePath + " already running, not running another one.";
+                console.log(msg);
+                res.writeHead(102); //102 = processing
+                res.end(msg);
+            } else {
+                batchFileAlreadyRunning = true;
+                //run bat file: 
+                //https://stackoverflow.com/a/36601805/51358           
+                child_process.exec(batchFilePath, function (error, stdout, stderr) {
+                    console.log(stdout);
+                    if (error) {
+                        const msg = 'Error executing ' + batchFilePath + ".Error: " + error;
+                        console.log(msg);
+                        res.writeHead(500);
+                        return res.end(msg);
+                    }
 
-                //send file to client for download: 
-                //https://stackoverflow.com/questions/21578208/node-js-send-file-to-client
-                const fileName = 'fileToDownload.exe';
-                var filePath = path.join(__dirname, fileName);
-                var stat = fs.statSync(filePath);
-                var file = fs.readFileSync(filePath, 'binary');
+                    //send file to client for download: 
+                    //https://stackoverflow.com/questions/21578208/node-js-send-file-to-client
+                    const fileName = 'fileToDownload.exe';
+                    var filePath = path.join(__dirname, fileName);
+                    var stat = fs.statSync(filePath);
+                    var file = fs.readFileSync(filePath, 'binary');
 
-                res.setHeader('Content-Length', stat.size);
-                res.setHeader('Content-Type', 'application/octet-stream');
-                res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
-                res.write(file, 'binary');
+                    res.setHeader('Content-Length', stat.size);
+                    res.setHeader('Content-Type', 'application/octet-stream');
+                    res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
+                    res.write(file, 'binary');
 
-                console.log(fileName + ' sent to client for download.');
+                    console.log(fileName + ' sent to client for download.');
 
-                res.end();
-            });
+                    res.end();
+                    batchFileAlreadyRunning = false;
+                });
+            }
             break;
     }
 });
